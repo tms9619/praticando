cfc57195e812c2e56c8cfdbb2780ded78ff5bea1
Thiago <thiagomarinhotms@gmail.com>
2021-07-20 23:01:45 -0300

    index alterada

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php 
 include "Class/Controller.php";
 
-// alterado
-
 $e1 = new Estoque('Sapato', 50 , 'Tipo A', 50);
 $teste = new Teste;
 
 echo $e1->getDetalhesProduto() ."<br/>". $e1->getDetalhesEstoque(). "<br/>". $teste->teste();
 echo Teste::fonfon();
 
+// Alteração feita
 ?>
